e48b771eccd2371be1b1e22cc2f7c5b9478796f8
Artem Zhukov <green.azhukov@gmail.com>
2017-03-31 10:52:08 +0200

    polished setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='myzcat',
+    version='0.1beta',
+    url='https://github.com/ZhukovGreen/psychrometrics',
+    license='GNU',
+    author='',
+    author_email='',
+    description='psychrometrics implementation',
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={},
+    entry_points={}
+)
